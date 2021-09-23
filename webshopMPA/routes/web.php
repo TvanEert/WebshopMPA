ab6d4a7a71bd1f6d81b07cb2b93d21f7f7b385e8
Tim van Eert <timvaneert@upcmail.nl>
2021-09-23 12:35:37 +0200

    Created Category model and controller

--- webshopMPA/routes/web.php
+++ webshopMPA/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/category', [CategoryContoller::class, 'index']);
+
+Route::get('/', function() { return view('home'); });
