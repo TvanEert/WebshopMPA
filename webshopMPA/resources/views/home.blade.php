34d2b2673f73a4b66f74f56f96ad181606899e60
Tim van Eert <timvaneert@upcmail.nl>
2021-10-07 14:02:00 +0200

    added img carousel

--- webshopMPA/resources/views/home.blade.php
+++ webshopMPA/resources/views/home.blade.php
@@ -1,6 +1,28 @@
 @include('includes.header')
-    @include('includes.navbar')
-    @foreach ($allCategories as $category)
-        <p>{{$category->category}} </p>
-    @endforeach
+@include('includes.navbar')
+<div class="container-fluid text-center">
+    <div class="container w-50 h-50">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ url('img/deathloop.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    </div>
+</div>
 @include('includes.footer')
