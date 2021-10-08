f02271a15b315b35eb631b4cbf5628d4306db46d
Tim van Eert <timvaneert@upcmail.nl>
2021-10-08 11:41:52 +0200

    Added carousel images

--- webshopMPA/resources/views/home.blade.php
+++ webshopMPA/resources/views/home.blade.php
@@ -5,13 +5,13 @@
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
+                <img src="{{ url('image/battlefield2042.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{ url('img/deathloop.jpg') }}" class="d-block w-100" alt="...">
+                <img src="{{ url('image/deathloop.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
+                <img src="{{ url('image/deadbydaylight.jpg') }}" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
