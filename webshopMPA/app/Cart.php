19e87aadae1ed7d55a810da753ad194d3f4b3d12
Tim van Eert <timvaneert@upcmail.nl>
2021-11-24 11:27:29 +0100

    Created Shopping cart class and controller

---
+++ webshopMPA/app/Cart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class Cart
+{
+    public $items = null;
+    public $totalQty = 0;
+    public $totalPrice = 0;
+
+    public function __construct($oldCart){
+        if($oldCart){
+            $this->items = $oldCart->items;
+            $this->totalQty = $oldCart->totalQty;
+            $this->totalPrice = $oldCart->totalPrice;
+        }
+    }
+}
