9b7494b6f3cb98823c49008abc3fdefc97a78c5b
Tim van Eert <timvaneert@upcmail.nl>
2021-11-26 09:42:49 +0100

    Preplaced function names

--- webshopMPA/app/Cart.php
+++ webshopMPA/app/Cart.php
@@ -4,15 +4,23 @@ namespace App;
 
 class Cart
 {
-    public $items = null;
+    public $products = null;
     public $totalQty = 0;
     public $totalPrice = 0;
 
     public function __construct($oldCart){
         if($oldCart){
-            $this->items = $oldCart->items;
+            $this->products = $oldCart->prducts;
             $this->totalQty = $oldCart->totalQty;
             $this->totalPrice = $oldCart->totalPrice;
         }
+    }
+
+    public function addToCart(){
+
+    }
+
+    public function removeFromCart(){
+
     }
 }
--- webshopMPA/app/Http/Controllers/CartController.php
+++ webshopMPA/app/Http/Controllers/CartController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cart;
 
 class CartController extends Controller
 {
-    //
+    public function getAllProductsFromCart(){
+
+    }
+
+    public function addProductToCart(){
+
+    }
+
+    public function removeProductFromCart(){
+
+    }
+
+    public function changeProductAmountInCart(){
+
+    }
 }
