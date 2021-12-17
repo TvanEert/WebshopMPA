5a8e259b9de86a5d33a0fc89f8d10a790038e534
Tim van Eert <timvaneert@upcmail.nl>
2021-12-17 09:57:46 +0100

    Revert "Added the add to cart function and save to session"
    This reverts commit 421c0ece4a81a1e2ca9c7b0173df8af34d90caba.

--- webshopMPA/app/Cart.php
+++ webshopMPA/app/Cart.php
@@ -8,42 +8,19 @@ class Cart
     public $totalQty = 0;
     public $totalPrice = 0;
 
-    public function __construct(Request $request){
-        $oldcart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
-
-        if($oldcart){
-            $this->products = $oldcart->products;
-            $this->totalQty = $oldcart->totalQty;
-            $this->totalPrice = $oldcart->totalPrice;
-        }  
-
-        $this->save();
+    public function __construct($oldCart){
+        if($oldCart){
+            $this->products = $oldCart->prducts;
+            $this->totalQty = $oldCart->totalQty;
+            $this->totalPrice = $oldCart->totalPrice;
+        }
     }
 
-    public function save(){
-        if(count($this->products) > 0){
-            session()->put('cart', $this);
-        } else {
-            session()->forget('cart');
-        }
+    public function addToCart(){
 
-    }
-
-    public function addToCart($product, $id){ 
-        $storedProducts = ['qty' => 0, 'price' => $product->price, 'product' => $product];
-        if ($this->products) {
-            if (array_key_exists($id, $this->products)) {
-                $storedProducts = $this->products[$id];
-            }
-        }
-        $storedProducts['qty']++;
-        $storedProducts['price'] = $product->price * $storedProducts['qty'];
-        $this->products[$id] = $storedProducts;
-        $this->totalQty++;
-        $this->totalPrice += $product->price;
     }
 
     public function removeFromCart(){
-        
+
     }
 }
